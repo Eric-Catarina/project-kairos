0dc7cd4c6cb92cb97e2abddf2f6427114b4a0f64
CaioFreitasRibeiro <136375048+CaioFreitasRibeiro@users.noreply.github.com>
2025-10-19 18:16:46 -0300

    MainMenu funcional

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,6 +11,8 @@ public class UIManager : MonoBehaviour
 
     public event Action<UIPanelType, bool> OnPanelStateChanged; // bool: true para mostrado, false para escondido
 
+    private GameObject activePanel;
+
     private readonly Dictionary<UIPanelType, UIPanel> _registeredPanels = new Dictionary<UIPanelType, UIPanel>();
     
     [Header("Debug Info")]
@@ -122,5 +124,25 @@ public class UIManager : MonoBehaviour
     public void CloseSettingsPanel()
     {
         ClosePanel(UIPanelType.Settings);
+    }
+
+    public void SetActivePanel(GameObject newPanel)
+    {
+        if (activePanel != null)
+            activePanel.SetActive(false);
+
+        newPanel.SetActive(true);
+        activePanel = newPanel;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
     }
 }
