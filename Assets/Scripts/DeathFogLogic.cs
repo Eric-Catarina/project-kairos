7ae5449b1f18c86dfcd9fd3e1219d3eb0ef093b6
Éric Catarina Parreiras <orment.cp@gmail.com>
2025-11-13 17:00:29 -0300

    fix: death fog

--- Assets/Scripts/DeathFogLogic.cs
+++ Assets/Scripts/DeathFogLogic.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DeathFogLogic : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
